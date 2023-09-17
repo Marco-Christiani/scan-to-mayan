a29a48093172777320b2ac894e4e21d4c5505464
Marco Christiani <mchristiani2017@gmail.com>
2023-09-16 23:00:45 -0400

    nicer
    Signed-off-by: Marco Christiani <mchristiani2017@gmail.com>

--- go-native-src/cmd/main.go
+++ go-native-src/cmd/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
 )
 
 var tmpStagingDir string = "/tmp/scan-to-mayan"
@@ -28,15 +30,15 @@ func init() {
 		log.Fatal("Environment variable MAYAN_WATCH_DIR not set")
 	}
 
+	boundList = binding.BindStringList(&pathList)
+
 	matches, err := filepath.Glob(filepath.Join(tmpStagingDir, "*"))
 	if err != nil {
 		log.Fatalf("Failed to glob: %v", err)
 	} else {
 		log.Printf("Found existing files in tmpStagingDir=%v so adding those to pathList\n", tmpStagingDir)
 	}
-	pathList = matches
-
-	boundList = binding.BindStringList(&pathList)
+	boundList.Set(matches)
 }
 
 func main() {
@@ -57,8 +59,8 @@ func main() {
 
 	submit = widget.NewButton("Submit", func() {
 		println("submit")
-		println("Out:", combineScans())
-		if len(pathList) == 0 { // all docs were correctly combined and pathList was emptied
+		combineScans()
+		if boundList.Length() == 0 { // all docs were correctly combined and pathList was emptied
 			submit.Disable()
 		}
 	})
@@ -69,7 +71,7 @@ func main() {
 	add = widget.NewButton("Add Page", func() {
 		add.Disable()
 		submit.Disable()
-		fname := fmt.Sprintf("filescan-%d.tiff", len(pathList))
+		fname := fmt.Sprintf("filescan-%d.tiff", boundList.Length())
 		progressBar.Show()
 		val := scanToFile(fname)
 		progressBar.Hide()
@@ -83,17 +85,25 @@ func main() {
 	myWindow.ShowAndRun()
 }
 
-func combineScans() string {
-	// TODO: this should be the mayan staging dir to output to
-	args := append(pathList, filepath.Join(tmpStagingDir, "output.pdf"))
+func combineScans() {
+	items, err := boundList.Get()
+	if err != nil {
+		log.Fatalf("Failed to get items of bound list: %v", err)
+	}
+	now := time.Now().Format("2006-01-02_15:04:05")
+	outputFname := fmt.Sprintf("output-%v.pdf", now)
+	args := append(items, filepath.Join(tmpStagingDir, outputFname))
 	cmd := exec.Command("/usr/bin/convert", args...)
-	println(cmd.String())
+	log.Printf("Running: %v", cmd.String())
 
-	out, err := cmd.Output()
+	err = cmd.Run()
 	if err != nil {
 		log.Fatalf("Command failed: %v", err)
+	} else {
+		log.Println("Success")
+		// TODO: delete the files
+		boundList.Set([]string{})
 	}
-	return string(out)
 }
 
 func scanToFile(fname string) string {
